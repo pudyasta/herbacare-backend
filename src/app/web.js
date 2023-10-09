b8f550c9c4d301b5c34f084d5c847a0f480ef8b2
pudyasta <pudyastasatria@gmail.com>
2023-10-09 21:59:09 +0700

    fix(cors)

--- src/app/web.js
+++ src/app/web.js
@@ -2,8 +2,9 @@ import express from "express";
 import { publicRouter } from "../routes/public-api.js";
 import { errorMiddleware } from "../middleware/errorMiddleware.js";
 import { userRouter } from "../routes/api.js";
-
+import cors from "cors";
 export const web = express();
+web.use(cors());
 web.use(express.json());
 web.use(publicRouter);
 web.use(userRouter);
