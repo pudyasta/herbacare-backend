8b578af1692a9bba0a03397fd145f390748390ca
pudyasta <pudyastasatria@gmail.com>
2023-10-10 14:02:02 +0700

    fix(article_id)

--- src/service/articleService.js
+++ src/service/articleService.js
@@ -30,7 +30,7 @@ const createArticleService = async (req) => {
 const getAllArticleService = async (req) => {
   const articles = await prismaClient.articles.findMany({
     take: 50,
-    select: { title: true, image: true, category: true },
+    select: { articles_id: true, title: true, image: true, category: true },
   });
   return articles;
 };
