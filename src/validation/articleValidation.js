4ff3e9e448ae8c8f4540efbae98c71fd50014840
pudyasta <pudyastasatria@gmail.com>
2023-10-10 13:47:40 +0700

    fix(longer article title)

--- src/validation/articleValidation.js
+++ src/validation/articleValidation.js
@@ -1,7 +1,7 @@
 import Joi from "joi";
 
 const createArticleValidation = Joi.object({
-  title: Joi.string().required().max(30),
+  title: Joi.string().required().max(80),
   body: Joi.string().required().max(1000),
   category_id: Joi.required(),
 });
